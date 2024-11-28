c9cb016ab08909a045bdfccea7e249e7cadef67e
Jimmy <x@201a.cn>
2024-11-29 00:04:59 +0800

    add feature description language

--- core/Parsing.Documents.fs
+++ core/Parsing.Documents.fs
@@ -170,6 +170,7 @@ let mutable languages = [
     lang "MATLAB" "" "" <| sc [line "%(?![%{}])"; block ("%\{", "%\}")]
     lang "Objective-C" "" ".m|.mm"
         java
+    lang "FeatureDescription" "feature-description" "" <| sc [line "#"]
     lang "OCaml" "ocaml|ocaml.interface" ".ml|.mli" <| sc [block (@"(.*\(\*+_? )", @"\*\)")]
     lang "Octave" "" "" <| sc [block ("#\{", "#\}"); block ("%\{", "%\}"); line "##?"; line "%[^!]"]
     lang "Pascal" "delphi" ".pas" <| sc [block (@"\(\*", @"\*\)"); block (@"\{(?!\$)", @"\}"); line "///?"]
